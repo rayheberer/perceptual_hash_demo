20f23e49dbd19877a16964bdb9e438bf6aae6ca7
Ray Heberer <ray.heberer@gmail.com>
2017-12-03 21:23:34 +0100

    save resized img

--- demo.R
+++ demo.R
@@ -31,7 +31,8 @@ bts1.pinknoise <- imgnoise::imgnoise(bts1, 'uniform', -0.1, 0.1)
 
 bts1.SnPnoise <- imgnoise(bts1, 'salt_and_pepper')
 
-OpenImageR::writeImage(bts1.rotated, './img/bts1_rotated.jpg')
+OpenImageR::writeImage(bts1.resized, './img/bts1_resized.jpg')
+writeImage(bts1.rotated, './img/bts1_rotated.jpg')
 writeImage(bts1.pinknoise, './img/bts1_pinknoise.jpg')
 writeImage(bts1.SnPnoise, './img/bts1_SnPnoise.jpg')
 
